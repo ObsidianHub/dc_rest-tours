4aa99534d5c207ab11146191ed34caa2c6d21812
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-06-17 15:08:35 +0300

    add new images and refactor code in js

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,17 @@
-(function() {
+(function () {
+  let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close");
 
-    let more = document.querySelector('.more'),
-        overlay = document.querySelector('.overlay'),
-        close = document.querySelector('.popup-close');
+  more.addEventListener("click", function () {
+    this.classList.add("more-splash");
+    overlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+  });
 
-        more.addEventListener('click', function() {
-            this.classList.add('more-splash');
-            overlay.style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        });
-
-        close.addEventListener('click', function() {
-            overlay.style.display = 'none';
-            more.classList.remove('more-splash');
-            document.body.style.overflow = '';
-        });
-
-})()
+  close.addEventListener("click", function () {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+  });
+})();
