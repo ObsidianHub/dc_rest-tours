19b56e2b42b8541b0815b5f909bc211118bdd22a
Denny <obsidian.y.dragon@gmail.com>
2020-08-07 14:59:17 +0300

    create variables for tabs manage

---
+++ js/tabs.js
@@ -0,0 +1,5 @@
+(function () {
+  let tab = document.getElementsByClassName("info-header-tab"),
+    tabContent = document.getElementsByClassName("info-tabcontent"),
+    info = document.querySelector(".info-header");
+})();
