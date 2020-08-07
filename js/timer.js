d1537dd7b80048aa0dcb31e025fa5e9ea7d6f5ae
Denny <obsidian.y.dragon@gmail.com>
2020-08-07 19:38:54 +0300

    create variables for all timing elements

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,12 @@
 (function () {
   let deadline = "2021-01-01";
 
-  function setClock(id, endtime) {}
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector(".hours"),
+      minutes = timer.querySelector(".minutes"),
+      seconds = timer.querySelector(".seconds");
+  }
 
   setClock("timer", deadline);
 })();
