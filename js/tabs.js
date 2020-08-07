b679d1a04438b3e3d861f1df75a6e4b0b7720ffa
Denny <obsidian.y.dragon@gmail.com>
2020-08-07 18:48:27 +0300

    add click listener for info

--- js/tabs.js
+++ js/tabs.js
@@ -2,4 +2,22 @@
   let tab = document.getElementsByClassName("info-header-tab"),
     tabContent = document.getElementsByClassName("info-tabcontent"),
     info = document.querySelector(".info-header");
+
+  console.log(tab[3]);
+
+  function hideTabContent() {}
+
+  function showTabContent() {}
+
+  info.addEventListener("click", function (e) {
+    let target = e.target;
+    if (target.className == "info-header-tab") {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          showTabContent(i);
+          break;
+        }
+      }
+    }
+  });
 })();
