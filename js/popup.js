634701851dd4918c673fca66a6be4f8d23a6d223
Denny <obsidian.y.dragon@gmail.com>
2020-08-11 00:58:27 +0300

    add listener for more button, display our overlay

--- js/popup.js
+++ js/popup.js
@@ -2,4 +2,10 @@
   let more = document.querySelector(".more"),
     overlay = document.querySelector(".overlay"),
     close = document.querySelector(".popup-close");
+
+  more.addEventListener("click", function () {
+    this.classList.add("more-splash");
+    overlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+  });
 })();
