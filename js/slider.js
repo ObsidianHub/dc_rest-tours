1f431fdcda86aaeea34515f2595bdc09b206904d
Denny <obsidian.y.dragon@gmail.com>
2020-08-10 23:29:01 +0300

    created all variables that we need

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+(function () {
+  let slideIndex = 1,
+    slides = document.getElementsByClassName("slider-item"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    dotsWrap = document.querySelector(".slider-dots"),
+    dots = document.getElementsByClassName("dot");
+})();
