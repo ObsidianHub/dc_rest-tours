66efa8897d8e776725bbda3cccd61229d66fd5e0
Denny <obsidian.y.dragon@gmail.com>
2020-08-11 11:51:13 +0300

    create clickHandler function

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -4,4 +4,15 @@
   for (const link of links) {
     link.addEventListener("click", clickHandler);
   }
+
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  }
 })();
