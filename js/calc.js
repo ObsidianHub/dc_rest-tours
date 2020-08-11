96141a188c5aab8501c899b6fbf1970b89e6edd2
Denny <obsidian.y.dragon@gmail.com>
2020-08-11 11:41:20 +0300

    add change listener for person quantity input

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,21 @@
 (function () {
   let persons = document.getElementsByClassName("counter-block-input")[0],
     restDays = document.getElementsByClassName("counter-block-input")[1],
-    selectField = document.getElementById("select"),
+    place = document.getElementById("select"),
     totalValue = document.getElementById("total"),
     personsSum = 0,
     daysSum = 0,
     total = 0;
+
+  totalValue.innerHTML = 0;
+
+  persons.addEventListener("change", function () {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 400;
+    if (restDays.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
 })();
